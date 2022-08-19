59ae08b7b5158cad67835572403175c01438c54c
zjs6631 <zacharyjonessmith@gmail.com>
2022-08-19 13:43:06 -0400

    added better picture navigation

--- main.js
+++ main.js
@@ -32,30 +32,87 @@ let pics = ["./images/pic1.jpg", "./images/pic2.jpg", "./images/pic3.jpg", "./im
 //picDiv.appendChild(pictureElement);
 
 //currPic watches index of pic being displayed
-let currPic = 0; 
+let currPicIndex = 0; 
 //grab the displayed img and the left right nav arrows
 let currImage = document.getElementById('displayedImg');
 const left = document.getElementById('left');
 const right = document.getElementById('right');
+const dots = document.getElementsByClassName('dot');
 //add logic for listeners to allow navigation through the pictures
 left.addEventListener('click', ()=>{
-    if(currPic == 0){
-        currPic = 5;
-        currImage.setAttribute('src', pics[currPic]);
+    dots[currPicIndex].style.backgroundColor = "white";
+    if(currPicIndex == 0){
+        
+        currPicIndex = 5;
+        currImage.setAttribute('src', pics[currPicIndex]);
+        dots[currPicIndex].style.backgroundColor = "black";
     } else {
-        currPic-=1;
-        currImage.setAttribute('src', pics[currPic]);
+        currPicIndex-=1;
+        currImage.setAttribute('src', pics[currPicIndex]);
+        dots[currPicIndex].style.backgroundColor = "black";
     }
 })
 
 right.addEventListener('click', ()=>{
-    if(currPic == 5){
-        currPic = 0;
-        currImage.setAttribute('src', pics[currPic]);
+    dots[currPicIndex].style.backgroundColor = "white";
+    if(currPicIndex == 5){
+        currPicIndex = 0;
+        currImage.setAttribute('src', pics[currPicIndex]);
+        
     } else {
-        currPic+=1;
-        currImage.setAttribute('src', pics[currPic]);
+        currPicIndex+=1;
+        currImage.setAttribute('src', pics[currPicIndex]);
     }
+    dots[currPicIndex].style.backgroundColor = "black";
+})
+
+//grab each pic element that is shown on the bottom pic nav bar
+
+const pic1 = document.getElementById('pic1');
+const pic2 = document.getElementById('pic2');
+const pic3 = document.getElementById('pic3');
+const pic4 = document.getElementById('pic4');
+const pic5 = document.getElementById('pic5');
+const pic6 = document.getElementById('pic6');
+
+//add event listeners to allow navigation to that specific picture
+//change the currPicIndex and dot colors accordingly
+pic1.addEventListener('click', ()=>{
+    dots[currPicIndex].style.backgroundColor = 'white';
+    currPicIndex = 0
+    dots[currPicIndex].style.backgroundColor = 'black';
+    currImage.setAttribute('src', './images/pic1.jpg');
+})
+
+pic2.addEventListener('click', ()=>{
+    dots[currPicIndex].style.backgroundColor = 'white';
+    currPicIndex = 1
+    dots[currPicIndex].style.backgroundColor = 'black';
+    currImage.setAttribute('src', './images/pic2.jpg');
+})
+pic3.addEventListener('click', ()=>{
+    dots[currPicIndex].style.backgroundColor = 'white';
+    currPicIndex = 2
+    dots[currPicIndex].style.backgroundColor = 'black';
+    currImage.setAttribute('src', './images/pic3.jpg');
+})
+pic4.addEventListener('click', ()=>{
+    dots[currPicIndex].style.backgroundColor = 'white';
+    currPicIndex = 3
+    dots[currPicIndex].style.backgroundColor = 'black';
+    currImage.setAttribute('src', './images/pic4.jpg');
+})
+pic5.addEventListener('click', ()=>{
+    dots[currPicIndex].style.backgroundColor = 'white';
+    currPicIndex = 4
+    dots[currPicIndex].style.backgroundColor = 'black';
+    currImage.setAttribute('src', './images/pic5.jpg');
+})
+pic6.addEventListener('click', ()=>{
+    dots[currPicIndex].style.backgroundColor = 'white';
+    currPicIndex = 5
+    dots[currPicIndex].style.backgroundColor = 'black';
+    currImage.setAttribute('src', './images/pic6.jpg');
 })
 
 
